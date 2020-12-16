e9ee5b5696d95cff8e902cdcadf916a2f4c703d4
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2020-12-16 10:17:21 +0100

    Mer fancy loginside som defaulter til et fast fnr fra mock-alt.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,23 +1,45 @@
-import { Hovedknapp } from 'nav-frontend-knapper';
+import {Hovedknapp} from 'nav-frontend-knapper';
 import Panel from 'nav-frontend-paneler';
-import { Input, Select, SkjemaGruppe } from 'nav-frontend-skjema';
-import { Sidetittel } from 'nav-frontend-typografi';
-import React, { useEffect, useState } from 'react';
-import { getMockAltApiURL } from '../../utils/restUtils';
+import {Input, Select, SkjemaGruppe} from 'nav-frontend-skjema';
+import {Sidetittel} from 'nav-frontend-typografi';
+import React, {useEffect, useState} from 'react';
+import {getMockAltApiURL} from '../../utils/restUtils';
+
+interface BrukerInfo {
+    navn: string,
+    fnr: string,
+}
 
 export const Login = () => {
     const [fnr, setFnr] = useState('');
     const [mockAltAlleFnr, setMockAltAlleFnr] = useState([]);
+    const [mockAltDefaultFnr, setMockAltDefaultFnr] = useState<string |undefined>(undefined);
+    const [mockAltTilfeldigFnr, setMockAltTilfeldigFnr] = useState<string |undefined>(undefined);
 
     const href = window.location.href;
     const params = href.slice(href.indexOf('?') + 1, href.length);
     console.log('params', params);
 
     useEffect(() => {
+        fetch(`${getMockAltApiURL()}/fiks/fast/fnr`)
+            .then((response) => response.text())
+            .then((text) => {
+                setMockAltDefaultFnr(text);
+                if(fnr.length < 1) {
+                    setFnr(text);
+                }
+            });
+        fetch(`${getMockAltApiURL()}/fiks/tilfeldig/fnr`)
+            .then((response) => response.text())
+            .then((text) => setMockAltTilfeldigFnr(text));
         fetch(`${getMockAltApiURL()}/fiks/alle/fnr`)
             .then((response) => response.json())
             .then((json) => setMockAltAlleFnr(json));
     }, []);
+    var brukerListe:Array<BrukerInfo> = [];
+    if(mockAltDefaultFnr) brukerListe.push({navn:"Default", fnr:mockAltDefaultFnr});
+    mockAltAlleFnr.forEach((it) => {brukerListe.push({navn:it, fnr:it})})
+    if(mockAltTilfeldigFnr) brukerListe.push({navn:"Nytt personnummer", fnr:mockAltTilfeldigFnr});
 
     const handleOnClick = () => {
         window.location.href = `${getMockAltApiURL()}/login/cookie?subject=${fnr}&${params}`;
@@ -26,23 +48,28 @@ export const Login = () => {
         <Panel border>
             <Sidetittel>Login</Sidetittel>
             <SkjemaGruppe legend="">
-                <Input onChange={(event) => setFnr(event.target.value)} label="BrukerID" />
-                {mockAltAlleFnr && mockAltAlleFnr.length > 0 && (
+                <Input
+                    onChange={(event) => setFnr(event.target.value)}
+                    label="BrukerID"
+                />
+                {brukerListe && brukerListe.length > 0 && (
                     <Select
                         onChange={(event) => setFnr(event.target.value)}
                         label="Velg fra forhåndsdefinerte brukerIDer"
                     >
-                        {mockAltAlleFnr.map((fnr) => {
+                        {brukerListe.map((bruker) => {
                             return (
-                                <option key={fnr} value={fnr}>
-                                    {fnr}
+                                <option key={bruker.navn} value={bruker.fnr} selected={bruker.fnr === fnr}>
+                                    {bruker.navn + " (" + bruker.fnr + ")"}
                                 </option>
                             );
                         })}
                     </Select>
                 )}
             </SkjemaGruppe>
-            <Hovedknapp onClick={() => handleOnClick()}>Generer token og gå tilbake til applikasjon</Hovedknapp>
+            <Hovedknapp onClick={() => handleOnClick()}>
+                Generer token for "{fnr}" og gå tilbake til applikasjon
+            </Hovedknapp>
         </Panel>
     );
 };
