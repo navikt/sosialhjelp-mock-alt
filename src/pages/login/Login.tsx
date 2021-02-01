3ded32311c9ae93a6cab8a22347651ed7562b48e
Per-Christian Nielsen <55442993+perchrnie@users.noreply.github.com>
2021-02-01 11:10:20 +0100

    Ny logginn side (#8)
    * Loginside som velger fra PDL personer.
    
    * Synker med enum i backend.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,15 @@
 import {Hovedknapp} from 'nav-frontend-knapper';
 import Panel from 'nav-frontend-paneler';
-import {Input, Select, SkjemaGruppe} from 'nav-frontend-skjema';
+import {Select, SkjemaGruppe} from 'nav-frontend-skjema';
 import {Sidetittel} from 'nav-frontend-typografi';
 import React, {useEffect, useState} from 'react';
 import {addParams, getMockAltApiURL, getRedirectParams} from '../../utils/restUtils';
-
-interface BrukerInfo {
-    navn: string,
-    fnr: string,
-}
+import {Personalia} from "../person/PersonMockData";
+import {Link} from "react-router-dom";
 
 export const Login = () => {
     const [fnr, setFnr] = useState('');
-    const [mockAltAlleFnr, setMockAltAlleFnr] = useState([]);
-    const [mockAltDefaultFnr, setMockAltDefaultFnr] = useState<string |undefined>(undefined);
-    const [mockAltTilfeldigFnr, setMockAltTilfeldigFnr] = useState<string |undefined>(undefined);
+    const [personliste, setPersonListe] = useState<Personalia[]>([]);
 
     const params = getRedirectParams();
 
@@ -22,52 +17,40 @@ export const Login = () => {
         fetch(`${getMockAltApiURL()}/fiks/fast/fnr`)
             .then((response) => response.text())
             .then((text) => {
-                setMockAltDefaultFnr(text);
-                if(fnr.length < 1) {
-                    setFnr(text);
-                }
+                setFnr(text);
             });
-        fetch(`${getMockAltApiURL()}/fiks/tilfeldig/fnr`)
-            .then((response) => response.text())
-            .then((text) => setMockAltTilfeldigFnr(text));
-        fetch(`${getMockAltApiURL()}/fiks/alle/fnr`)
+        fetch(`${getMockAltApiURL()}/mock-alt/personalia/liste`)
             .then((response) => response.json())
-            .then((json) => setMockAltAlleFnr(json));
-    }, [fnr.length]);
-    var brukerListe:Array<BrukerInfo> = [];
-    if(mockAltDefaultFnr) brukerListe.push({navn:"Default", fnr:mockAltDefaultFnr});
-    mockAltAlleFnr.forEach((it) => {brukerListe.push({navn:it, fnr:it})})
-    if(mockAltTilfeldigFnr) brukerListe.push({navn:"Nytt personnummer", fnr:mockAltTilfeldigFnr});
+            .then((json) => setPersonListe(json));
+    }, []);
 
     const handleOnClick = () => {
         window.location.href = `${getMockAltApiURL()}/login/cookie?subject=${fnr}${addParams(params,"&")}`;
     };
+
     return (
         <Panel border>
-            <Sidetittel>Login</Sidetittel>
+            <Sidetittel>Mock login</Sidetittel>
             <SkjemaGruppe legend="">
-                <Input
-                    onChange={(event) => setFnr(event.target.value)}
-                    label="BrukerID"
-                />
-                {brukerListe && brukerListe.length > 0 && (
                     <Select
                         onChange={(event) => setFnr(event.target.value)}
-                        label="Velg fra forhåndsdefinerte brukerIDer"
+                        label="Velg bruker"
+                        value={fnr}
                     >
-                        {brukerListe.map((bruker) => {
+                        {personliste.map((bruker) => {
                             return (
-                                <option key={bruker.navn} value={bruker.fnr} selected={bruker.fnr === fnr}>
-                                    {bruker.navn + " (" + bruker.fnr + ")"}
+                                <option key={bruker.fnr} value={bruker.fnr}>
+                                    {bruker.navn.fornavn + " " + bruker.navn.mellomnavn + " " + bruker.navn.etternavn + " (" + bruker.fnr + ")"}
                                 </option>
                             );
                         })}
                     </Select>
-                )}
             </SkjemaGruppe>
             <Hovedknapp onClick={() => handleOnClick()}>
-                Generer token for "{fnr}" og gå tilbake til applikasjon
+                Login
             </Hovedknapp>
+            <Link className="knapp leftPadding" to={"/person" + addParams(params)} type="knapp">Lag ny bruker</Link>
+            <Link className="knapp leftPadding" to={"/" + addParams(params)}>Gå til oversikten</Link>
         </Panel>
     );
 };
