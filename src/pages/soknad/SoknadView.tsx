8ee4ee02eb9702139e6424dabe36de7f43dfd1fa
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-03-14 17:27:17 +0100

    Vis checksum for soknadJSON

--- src/pages/soknad/SoknadView.tsx
+++ src/pages/soknad/SoknadView.tsx
@@ -3,11 +3,15 @@ import {
     useGetSoknadJsonV2,
     useGetVedleggJsonV2,
 } from '../../generated/frontend-soknad-controller-v-2/frontend-soknad-controller-v-2';
-import { Button, Heading } from '@navikt/ds-react';
+import { Button, Detail, Heading } from '@navikt/ds-react';
 import ReactJson from '@microlink/react-json-view';
 import { ArrowLeftIcon } from '@navikt/aksel-icons';
+import hash from 'object-hash';
+import { useMemo } from 'react';
+import { format, parseJSON } from 'date-fns';
+import { nb } from 'date-fns/locale';
 
-const downloadSoknad = (data: object, label: string, soknadId: string) => {
+const downloadJson = (data: object, label: string, soknadId: string) => {
     const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -24,7 +28,10 @@ export const SoknadView = () => {
     if (!soknadId) throw new Error('Mangler søknad-ID i URL');
     const { data: soknadJson } = useGetSoknadJsonV2(soknadId);
     const { data: vedleggJson } = useGetVedleggJsonV2(soknadId);
-
+    const soknadJsonString = useMemo(
+        () => hash({ ...soknadJson, innsendingstidspunkt: undefined }, { encoding: 'hex' }),
+        [soknadJson]
+    );
     if (!soknadJson || !vedleggJson)
         return (
             <Heading level="1" size={'xlarge'}>
@@ -36,19 +43,36 @@ export const SoknadView = () => {
         <article className={'space-y-4'}>
             <Link to={'/soknader'}>
                 <div className={'flex items-center pb-2'}>
-                    <ArrowLeftIcon title="tilbake" fontSize={'1.5rem'} className={'inline'} />
+                    <ArrowLeftIcon title="tilbake" fontSize={'1.5rem'} className={'mr-1'} />
                     Tilbake til søknadsoversikt
                 </div>
             </Link>
-            <Heading level="1" size="xlarge">
+            <Heading level="2" size="xlarge">
                 Søknad {soknadId}
             </Heading>
             <section>
+                <table className={'w-full table-fixed m-2 [&_th]:w-60 [&_th]:px-1 [&_th]:text-right'}>
+                    <tbody>
+                        <tr>
+                            <th>Innsendingstidspunkt:</th>
+                            <td>{format(parseJSON(soknadJson.innsendingstidspunkt!), 'PPpp', { locale: nb })}</td>
+                        </tr>
+                        <tr>
+                            <th>Checksum soknadJSON:</th>
+                            <td>{soknadJsonString}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Detail>
+                    Checksum beregnes minus <code>innsendingstidspunkt</code>
+                </Detail>
+            </section>
+            <section>
                 <div className={'flex gap-4 items-center pb-2'}>
-                    <Heading level={'2'} size={'medium'}>
+                    <Heading level={'3'} size={'medium'}>
                         Søknad JSON
                     </Heading>
-                    <Button size={'small'} onClick={() => downloadSoknad(soknadJson, 'soknad', soknadId)}>
+                    <Button size={'small'} onClick={() => downloadJson(soknadJson, 'soknad', soknadId)}>
                         Last ned
                     </Button>
                 </div>
@@ -58,10 +82,10 @@ export const SoknadView = () => {
             </section>
             <section>
                 <div className={'flex gap-4 items-center pb-2'}>
-                    <Heading level={'2'} size={'medium'}>
+                    <Heading level={'3'} size={'medium'}>
                         Vedlegg JSON
                     </Heading>
-                    <Button size={'small'} onClick={() => downloadSoknad(vedleggJson, 'vedlegg', soknadId)}>
+                    <Button size={'small'} onClick={() => downloadJson(vedleggJson, 'vedlegg', soknadId)}>
                         Last ned
                     </Button>
                 </div>
