23fdcd4dec0649e76a0584b9bc694d7436b45ff5
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-10-30 13:12:56 +0100

    Gjør eslint.config.mjs mer lik tidligere config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,36 @@
-// @ts-check
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsLint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-);
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+    {
+        ignores: ["./dist"],
+    },
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            parser: tsParser,
+            globals: { ...globals.browser },
+        },
+        plugins: {
+            "@typescript-eslint": tsLint,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...tsLint.configs["recommended"].rules,
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+    },
+];
